642e477a0dbccbeb65cb379da14539ba405adfb1
Daniel Rossi <danielrossimacedo07@gmail.com>
2025-06-29 23:14:25 -0300

    Create 1_Two_Sum.cs

---
+++ Solutions/1_Two_Sum.cs
@@ -0,0 +1,25 @@
+﻿//https://leetcode.com/problems/two-sum/description/    
+
+public class Two_Sum
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var complements = new Dictionary<int, int>();
+
+        for (var pos = 0; pos < nums.Length; pos++)
+        {
+            var complement = target - nums[pos];
+
+            if (complements.ContainsKey(complement))
+            {
+                return new int[] { complements[complement], pos };
+            }
+            else
+            {
+                complements[nums[pos]] = pos;
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+}

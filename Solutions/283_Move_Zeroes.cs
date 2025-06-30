d5f8530e8c2f405b1f1c3595dbc451d3db0f3a1d
Daniel Rossi <danielrossimacedo07@gmail.com>
2025-06-29 23:14:31 -0300

    Create 283_Move_Zeroes.cs

---
+++ Solutions/283_Move_Zeroes.cs
@@ -0,0 +1,38 @@
+﻿//https://leetcode.com/problems/move-zeroes/description/
+
+public class Move_Zeroes
+{
+    public void MoveZeroes(int[] nums)
+    {
+        var currentIndex = 0;
+        var nonZeroIndex = 0;
+        var stop = false;
+
+        while (currentIndex < nums.Length)
+        {
+            if (nums[currentIndex] == 0)
+            {
+                stop = true;
+                for (var pos = currentIndex; pos < nums.Length; pos++)
+                {
+                    if (nums[pos] != 0)
+                    {
+                        nonZeroIndex = pos;
+                        stop = false;
+                        break;
+                    }
+                }
+                if (stop)
+                {
+                    return;
+                }
+                nums[currentIndex] = nums[nonZeroIndex];
+                nums[nonZeroIndex] = 0;
+            }
+            else
+            {
+                currentIndex += 1;
+            }
+        }
+    }
+}

e319e54daa8278f52d2daf3ddbe691150eefd802
Daniel Rossi <danielrossimacedo07@gmail.com>
2025-08-23 15:45:08 -0300

    Save progress - Daily temperatures

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//["MinStack","push","push","push","push","getMin","pop","getMin","pop","getMin","pop","getMin"]
+//[[],[2],[0],[3],[0],[],[],[],[],[],[],[]]
+//[null,null,null,null,null,0,null,0,null,0,null,2]
+
+//["MinStack","push","push","push","top","pop","getMin","pop","getMin","pop","push","top","getMin","push","top","getMin","pop","getMin"]
+//[[],[2147483646],[2147483646],[2147483647],[],[],[],[],[],[],[2147483647],[],[],[-2147483648],[],[],[],[]]
+
+
+int[] temperatures = new int[] { 73, 74, 75, 71, 69, 72, 76, 73 };
+new Daily_Temperatures().DailyTemperatures(temperatures);
---
+++ Solutions/739_Daily_Temperatures.cs
@@ -0,0 +1,34 @@
+﻿public class Daily_Temperatures
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        var results = new int[temperatures.Length];
+
+        for (var currentPos = 0; currentPos < temperatures.Length; currentPos++)
+        {
+            if (currentPos + 1 == temperatures.Length)
+            {
+                results[currentPos] = 0;
+                return results;
+            }
+
+            var noWarmerDay = true;
+            var currentTemp = temperatures[currentPos];
+            for (var pos = currentPos + 1; pos < temperatures.Length; pos++)
+            {
+                var nextTemp = temperatures[pos];
+                if (currentTemp < nextTemp)
+                {
+                    results[currentPos] = pos - currentPos;
+                    noWarmerDay = false;
+                    break;
+                }
+            }
+
+            if (noWarmerDay)
+                results[currentPos] = 0;
+        }
+
+        return results;
+    }
+}

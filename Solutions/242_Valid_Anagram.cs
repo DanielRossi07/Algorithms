ff73f2df119a3971f830d6190138ce1ac9a37d89
Daniel Rossi <danielrossimacedo07@gmail.com>
2025-10-15 22:51:40 -0300

    Feat: Adicionando melhores soluções pro problema do anagrama 242

--- Solutions/242_Valid_Anagram.cs
+++ Solutions/242_Valid_Anagram.cs
@@ -58,5 +58,52 @@ namespace Algorithms.Solutions
 
             return true;
         }
+
+        public bool IsAnagram_Bettter(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            var sHash = new Dictionary<char, int>();
+            var tHash = new Dictionary<char, int>();
+
+            for (int pos = 0; pos < s.Length; pos++)
+            {
+                if (sHash.TryGetValue(s[pos], out var countS))
+                    sHash[s[pos]] = countS + 1;
+                else
+                    sHash[s[pos]] = 1;
+
+                if (tHash.TryGetValue(t[pos], out var countT))
+                    tHash[t[pos]] = countT + 1;
+                else
+                    tHash[t[pos]] = 1;
+            }
+
+            foreach (var key in sHash.Keys)
+            {
+                if (!tHash.TryGetValue(key, out var tCount) || sHash[key] != tCount)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public bool IsAnagram_Best(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            var counts = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                counts[s[i]] = counts.GetValueOrDefault(s[i], 0) + 1;
+                counts[t[i]] = counts.GetValueOrDefault(t[i], 0) - 1;
+            }
+
+            return counts.Values.All(v => v == 0);
+        }
     }
+
 }

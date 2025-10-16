1c3c1a3e4a5af5826fff6a8c899712e3538d4375
Daniel Rossi <danielrossimacedo07@gmail.com>
2025-10-15 21:46:41 -0300

    Feat: Adicionando problema 217 Contains Duplicate

---
+++ Solutions/217_Contains_Duplicate.cs
@@ -0,0 +1,22 @@
+﻿//https://leetcode.com/problems/contains-duplicate/submissions/1802899568/
+namespace Algorithms.Solutions
+{
+    public class Contains_Duplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            var seenNumbers = new HashSet<int>();
+
+            foreach(var num in nums)
+            {
+                if (seenNumbers.Contains(num))
+                {
+                    return true;
+                }
+                seenNumbers.Add(num);
+            }
+
+            return false;
+        }
+    }
+}
